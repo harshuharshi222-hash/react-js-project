7c3bbaf9b3595621924894d9907692935a1e19a0
harshuharshi222-hash <harshuharshi222@gmail.com>
2026-05-25 11:18:31 +0530

    import  all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
 import MiniDrawer from './components/sideBar.jsx'
-
+import PromotionalActivity from './components/promotionalt.jsx'
+import './components/sideBar.css'
+import Dashboard from './components/dashboard.jsx'
+import CreateCL from './components/create.jsx'
+import Get from './components/get.jsx'
+import Update from './components/update.jsx'
 
 export default function App(){
 return(
--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -271,7 +271,7 @@ export default function MiniDrawer() {
                 <Get></Get> */}
 
                 {/* <Dashboard></Dashboard> */}
-                <PromotionalActivity></PromotionalActivity>
+                {/* <PromotionalActivity></PromotionalActivity> */}
 
             </Box>
         </Box >
